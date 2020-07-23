b6b2edf43f88f85aaf197a50eb45a5f95adb5961
Courtney Stowers <54308186+castower@users.noreply.github.com>
2020-07-23 01:33:57 -0700

    rock, paper, scissors game w/ loop
    - Rock, Paper, Scissors with loop and case insensitivity
    - My second Python project after completing freeCodeCamp beginner course on Youtube: https://www.youtube.com/watch?v=rfscVS0vtbw

---
+++ rock-paper-scissors-loop-cs.py
@@ -0,0 +1,52 @@
+# Courtney Stowers
+# July 23, 2020
+# Rock, Paper, Scissors with loop and case insensitivity
+# My second Python project after completing freeCodeCamp 
+# beginner course on Youtube: https://www.youtube.com/watch?v=rfscVS0vtbw
+
+import random  # use random module for computer to randomly select rock, paper, or scissors
+
+computer = ("rock", "paper", "scissors")  # random option list for computer's game selections
+
+
+def rock_paper_scissors():
+    player_select = input(
+        "Enter rock, paper, or scissors to begin game: ")  # prompt for player to enter their selection
+    selection = str(random.sample(computer, 1))[1:-1]  # computer's random selection, remove brackets
+    computer_select = selection.replace("'", "")  # remove quotation marks from selection
+    play_again = "yes"
+    while play_again == "yes":
+        if player_select.lower() == "rock" and computer_select == "scissors":
+            print("Player selection: " + str(player_select) + "\nComputer selection: " + str.strip(
+                computer_select) + "\nYou win! Congratulations!")  # str.strip removes quotes from output
+        elif player_select.lower() == "rock" and computer_select == "rock":
+            print("Player selection: " + str(player_select) + "\nComputer selection: " + str.strip(
+                computer_select) + "\nTie! Try again!")
+        elif player_select.lower() == "paper" and computer_select == "rock":
+            print("Player selection: " + str(player_select) + "\nComputer selection: " + str.strip(
+                computer_select) + "\nYou win! Congratulations!")
+        elif player_select.lower() == "paper" and computer_select == "paper":
+            print("Player selection: " + str(player_select) + "\nComputer selection: " + str.strip(
+                computer_select) + "\nTie! Try again!")
+        elif player_select.lower() == "scissors" and computer_select == "paper":
+            print("Player selection: " + str(player_select) + "\nComputer selection: " + str.strip(
+                computer_select) + "\nYou win! Congratulations!")
+        elif player_select.lower() == "scissors" and computer_select == "scissors":
+            print("Player selection: " + str(player_select) + "\nComputer selection: " + str.strip(
+                computer_select) + "\nTie! Try again!")
+        else:
+            print("Player selection: " + str(player_select) + "\nComputer selection: " + str.strip(
+                computer_select) + "\nYou lose! Try again!")
+        play_again = input("\nDo you want to try again? Enter yes or no: ")
+        if play_again.lower() == "yes":
+            rock_paper_scissors()
+            break
+        elif play_again.lower() == "no":
+            print("Aw, okay :( Hope you play again soon!")
+            break
+        else:
+            break
+
+
+# Play game:
+rock_paper_scissors()
