3db5a151c8060078e00f55aaf23b328543d60082
Courtney Stowers <54308186+castower@users.noreply.github.com>
2020-07-23 09:46:57 -0700

    game w/loop and case insensitivity
    Added if statements for wrong inputs.

--- rock-paper-scissors-loop-cs.py
+++ rock-paper-scissors-loop-cs.py
@@ -1,7 +1,7 @@
 # Courtney Stowers
 # July 23, 2020
 # Rock, Paper, Scissors with loop and case insensitivity
-# My second Python project after completing freeCodeCamp 
+# My second Python project after completing freeCodeCamp
 # beginner course on Youtube: https://www.youtube.com/watch?v=rfscVS0vtbw
 
 import random  # use random module for computer to randomly select rock, paper, or scissors
@@ -16,7 +16,9 @@ def rock_paper_scissors():
     computer_select = selection.replace("'", "")  # remove quotation marks from selection
     play_again = "yes"
     while play_again == "yes":
-        if player_select.lower() == "rock" and computer_select == "scissors":
+        if player_select.lower() != "rock" and player_select.lower() != "paper" and player_select.lower() != "scissors":
+            print("Wrong input! Please enter rock, paper, or scissors and try again!")
+        elif player_select.lower() == "rock" and computer_select == "scissors":
             print("Player selection: " + str(player_select) + "\nComputer selection: " + str.strip(
                 computer_select) + "\nYou win! Congratulations!")  # str.strip removes quotes from output
         elif player_select.lower() == "rock" and computer_select == "rock":
@@ -44,6 +46,18 @@ def rock_paper_scissors():
         elif play_again.lower() == "no":
             print("Aw, okay :( Hope you play again soon!")
             break
+        elif play_again.lower() != "yes" and play_again.lower() != "no":
+            print("Wrong input! Please enter yes or no.")
+            play_again = input("\nDo you want to try again? Enter yes or no: ")
+            if play_again.lower() == "yes":
+                rock_paper_scissors()
+                break
+            elif play_again.lower() == "no":
+                print("Aw, okay :( Hope you play again soon!")
+                break
+            else:
+                print("Wrong input! Try again later. Goodbye!")
+                break
         else:
             break
 
